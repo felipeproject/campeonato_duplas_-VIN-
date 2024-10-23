c1c5d4bf6aae2f6a2671d70d702bfdc20780188f
felipeproject <feelipe_andree@hotmail.com>
2024-10-23 06:15:12 -0300

    Atualizações automáticas

--- script.js
+++ script.js
@@ -1,47 +1,37 @@
 function carregarEquipes() {
-    fetch('equipes.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Rede não disponível');
-            }
-            return response.json();
-        })
-        .then(data => {
-            let admin = data.admin;
-            let adminContainer = document.getElementById('admin');
-            adminContainer.innerHTML = `
-                <div class="admin-container">
-                    <img src="imagens/${admin.imagem}" alt="${admin.nome}">
-                    <h2>${admin.nome}</h2>
-                </div>
-            `;
+    fetch('equipes.json').then(response => {
+        if (!response.ok) throw new Error('Rede não disponível');
+        return response.json();
+    }).then(data => {
+        let admin = data.admin;
+        let adminContainer = document.getElementById('admin');
+        adminContainer.innerHTML = `<div class="admin-container"><img src="imagens/${admin.imagem}" alt="${admin.nome}"><h2>${admin.nome}</h2></div>`;
+        let container = document.getElementById('equipesContainer');
+        data.equipes.forEach(equipe => {
+            const nick1 = equipe.dupla[0];
+            const nick2 = equipe.dupla[1];
 
-            let container = document.getElementById('equipesContainer');
-            data.equipes.forEach(equipe => {
-                const nick1 = equipe.dupla[0];
-                const nick2 = equipe.dupla[1];
-                const link1 = `https://pubg.op.gg/user/${nick1}`;
-                const link2 = nick2 === '(aguardando a dupla)' ? nick2 : `https://pubg.op.gg/user/${nick2}`;
+            // Remove a condição para "aguardando a dupla"
+            const link1 = `https://pubg.op.gg/user/${nick1}`;
+            const link2 = `https://pubg.op.gg/user/${nick2}`; // Não verifica mais se nick2 está aguardando
 
-                container.innerHTML += `
-                    <div class="dupla">
-                        <div class="card">
-                            <img src="imagens/${equipe.imagens[0]}" alt="${nick1}">
-                            <a href="${link1}" target="_blank" class="link-nome">${nick1}</a>
-                        </div>
-                        <div class="icon">
-                            <img src="imagens/icone_partido.png" alt="Ícone de Partido" class="partido-icon">
-                        </div>
-                        <div class="card">
-                            <img src="imagens/${equipe.imagens[1]}" alt="${nick2}" class="${nick2 === '(aguardando a dupla)' ? 'pulse' : ''}">
-                            <a href="${link2}" target="_blank" class="link-nome">${nick2}</a>
-                        </div>
+            container.innerHTML += `
+                <div class="dupla">
+                    <div class="card">
+                        <img src="imagens/${equipe.imagens[0]}" alt="${nick1}">
+                        <a href="${link1}" target="_blank" class="link-nome">${nick1}</a>
                     </div>
-                `;
-            });
+                    <div class="icon">
+                        <img src="imagens/icone_partido.png" alt="Ícone de Partido" class="partido-icon">
+                    </div>
+                    <div class="card">
+                        <img src="imagens/${equipe.imagens[1]}" alt="${nick2}">
+                        <a href="${link2}" target="_blank" class="link-nome">${nick2}</a>
+                    </div>
+                </div>`;
         });
+    });
 }
-
 carregarEquipes();
 
 function abrirWidget() {
@@ -64,7 +54,5 @@ function copiarChavePix() {
     document.body.removeChild(tempInput);
     var copyMessage = document.getElementById("copyMessage");
     copyMessage.style.display = "block";
-    setTimeout(function () {
-        copyMessage.style.display = "none";
-    }, 2000);
+    setTimeout(() => { copyMessage.style.display = "none"; }, 2000);
 }
--- server.bat
+++ server.bat
@@ -1,1 +1,2 @@
+python -m http.server 8081
 python -m http.server 8080
